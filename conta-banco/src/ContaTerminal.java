9c5caabb273ca106a8f33c8cb190db14c07c716c
Mateus F. Alves <44509797+matzaldev@users.noreply.github.com>
2024-08-24 20:14:40 -0300

    Conta banco criada com sucesso e testada tanto no terminal da IDE, quanto no MS-DOS.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale; 
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Conta banco!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US); //Criando um novo objeto Scanner e definindo a localização como US (United States)
+
+        System.out.println("Por favor, digite o seu nome: "); // Aqui estou imprimindo perguntas ao usuario
+        String nome = scanner.next(); // E aqui estou coletando as informações para imprimir no final do código.
+
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite a sua agência: ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Informe seu saldo atual: ");
+        double saldoAtual = scanner.nextDouble();
+
+        //Abaixo fiz um diálogo final, informando as informações do cliente e imprimindo uma mensagem de boas-vindas.
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + " Conta: " + numeroConta + "e seu saldo de: " + saldoAtual + " já está disponível para saque!");
+
     }
 }
+
